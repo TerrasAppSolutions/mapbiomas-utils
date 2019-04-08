2951d4c1a6571206c7f53d7e66102ca3f5d4b54e
Dyeden Monteiro <dyedenm@gmail.com>
2019-04-08 16:23:53 -0300

    primeira versão script para merge de imagens

--- download/merge_images.py
+++ download/merge_images.py
@@ -1,9 +1,46 @@
 
 import os
+import argparse
+
+def get_list_names_raster():
+    return ['AMAZONIA', 'CAATINGA', 'CERRADO', 'MATAATLANTICA', 'PAMPA', 'PANTANAL']
 
 def buildvrt(folder_path, biome):
     osCommand = "gdalbuildvrt  " + folder_path + "/" + biome + ".vrt "   + folder_path + "/" + biome + "*"
     os.system(osCommand)
 
-def merge_images(folder_path, biome)
+def merge_images(folder_path, biome):
     osCommand = 'gdal_translate -of GTiff -co "COMPRESS=LZW" ' + folder_path + "/" + biome + ".vrt " + folder_path + "/" + biome + ".tif"
+    os.system(osCommand)
+
+def execute(bioma, folder_vrt):
+
+    osCommand = "mkdir -p " + folder_vrt
+    os.system(osCommand)
+
+    if bioma == 'all':
+        
+        names = get_list_names_raster()
+
+        #execute vrt
+        for name in names:
+            print('executing vrt', name)
+            buildvrt(folder_vrt, name)
+
+        #execute merge
+        for name in names:
+            print('executing merge', name)
+            merge_images(folder_vrt, name)
+
+def interface():
+    parser = argparse.ArgumentParser(description='Merge the images for download')
+
+    parser.add_argument('name', type=str, help='choose the raster name', 
+                        choices=['AMAZONIA', 'CAATINGA', 'CERRADO', 'MATAATLANTICA', 
+                        'PAMPA', 'PANTANAL', 'all']) #TODO only 'all' works 
+
+    parser.add_argument('folder_vrt', type=str,  help='choose the vrt folder')
+
+    raster_name = parser.parse_args().name
+    
+
