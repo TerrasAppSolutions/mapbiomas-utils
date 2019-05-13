0e17f71a8c9d3485fd0a674a6d5c7345d8593647
Dyeden Monteiro <dyedenm@gmail.com>
2019-05-13 15:54:11 -0300

    chaco transicao

--- images/buildvrt_util.py
+++ images/buildvrt_util.py
@@ -1,12 +1,19 @@
 import os
 import argparse
-
+import json
 
 def get_integration_years():
 
     years = range(2010, 2018)
 
     return years
+
+def get_info_project(project_name):
+    if project_name == 'chaco':
+       data = json.load(open('info_chaco.json')) 
+    else:
+        raise Exception('this project name doesnt exist')
+    return data
 
 def vrt_integration(pathInput, pathOutput):
 
@@ -25,23 +32,17 @@ def vrt_integration(pathInput, pathOutput):
         print(osCommand)
         os.system(osCommand)
 
-def get_transition_years():
-    transition_years = [ u'1985_1986', u'1986_1987', u'1987_1988', u'1988_1989', u'1989_1990', u'1990_1991',
-                         u'1991_1992', u'1992_1993', u'1993_1994', u'1994_1995', u'1995_1996', u'1996_1997', 
-                         u'1997_1998', u'1998_1999', u'1999_2000', u'2000_2001', u'2001_2002', u'2002_2003', 
-                         u'2003_2004', u'2004_2005', u'2005_2006', u'2006_2007', u'2007_2008', u'2008_2009', 
-                         u'2009_2010', u'2010_2011', u'2011_2012', u'2012_2013', u'2013_2014', u'2014_2015', 
-                         u'2015_2016', u'2016_2017', u'1985_1990', u'1990_1995', u'1995_2000', u'2000_2005', 
-                         u'2005_2010', u'2010_2015', u'2015_2017', u'1990_2000', u'2000_2010', u'2010_2017', 
-                         u'1985_2017', u'2008_2017', u'2012_2017', u'1994_2002', u'2002_2010', u'2010_2016']
+def get_transition_years(project_name):
+
+    transition_years = get_info_project(project_name)["transition_years"]
 
     return transition_years
 
-def vrt_transition(pathInput, pathOutput):
+def vrt_transition(pathInput, pathOutput, project_name):
 
-    transitionYears = get_transition_years()
+    transitionYears = get_transition_years(project_name)
 
-    bands = range(0, len(get_transition_years()))
+    bands = range(0, len(transitionYears))
 
     for band in bands:
 
@@ -78,6 +79,8 @@ def interface():
 
     parser = argparse.ArgumentParser(description='Create VRT')
 
+    parser.add_argument('project', type=str, help='write the project name', choices=['brasil', 'chaco', 'raisg'])
+
     parser.add_argument('colecao', type=str, default='integracao', help='choose which collection', 
                     choices=['integracao', 'transicao', 'rgb'])
 
@@ -85,6 +88,7 @@ def interface():
 
     parser.add_argument('dir_dst', type=str,  help='the destination folder')
     
+    project_name = parser.parse_args().project
     colecao = parser.parse_args().colecao
     dir_src = parser.parse_args().dir_src
     dir_dst = parser.parse_args().dir_dst
@@ -93,7 +97,7 @@ def interface():
         vrt_integration(dir_src, dir_dst)
 
     if colecao == 'transicao':
-        vrt_transition(dir_src, dir_dst)
+        vrt_transition(dir_src, dir_dst, project_name)
 
     if colecao == 'rgb':
         vrt_rgb(dir_src, dir_dst)
