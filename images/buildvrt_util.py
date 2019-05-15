711758d70cb9c39152aea1820fecd3606a2afb2f
Dyeden Monteiro <dyedenm@gmail.com>
2019-05-15 10:06:57 -0300

    ajuste rgb chaco

--- images/buildvrt_util.py
+++ images/buildvrt_util.py
@@ -60,11 +60,11 @@ def vrt_rgb(pathInput, pathOutput):
 
     integrationYears = get_integration_years()
 
-    bands = range(0,33)
+    bands = range(0,len(integrationYears))
 
     for band in bands:
         year = str(integrationYears[band])
-        pathImageInput = pathInput + '/mosaic-rgb-collection3-' + year +  '*.tif'
+        pathImageInput = pathInput + '/mosaic-rgb-collection1-' + year +  '*.tif'
         pathImageOutput = pathOutput + "/RGB_" + str(integrationYears[band]) + '.vrt'
 
         osCommand = 'gdalbuildvrt -allow_projection_difference -overwrite ' + \
