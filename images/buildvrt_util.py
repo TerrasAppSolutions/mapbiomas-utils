b6a2011addcee1577f1883f7d273c76e160271ff
dyeden <dyedenm@gmail.com>
2019-08-20 16:19:47 -0300

    adjust info

--- images/buildvrt_util.py
+++ images/buildvrt_util.py
@@ -3,18 +3,16 @@ import argparse
 import json
 from info_lib import get_info_project
 
-def get_integration_years():
+def get_integration_years(info):
 
-    info = get_info_project(project_name)
-    info = [item for item in info if item['col'] == '4'][0]
     years = info["years"]
 
     return years
 
 
-def vrt_integration(pathInput, pathOutput):
+def vrt_integration(pathInput, pathOutput, info):
 
-    integrationYears = get_integration_years()
+    integrationYears = get_integration_years(info)
 
     bands = range(0,len(integrationYears))
 
@@ -29,15 +27,15 @@ def vrt_integration(pathInput, pathOutput):
         print(osCommand)
         os.system(osCommand)
 
-def get_transition_years(project_name):
+def get_transition_years(info):
 
-    transition_years = get_info_project(project_name)["transition_years"]
+    transition_years = info["transition_years"]
 
     return transition_years
 
-def vrt_transition(pathInput, pathOutput, project_name):
+def vrt_transition(pathInput, pathOutput, info):
 
-    transitionYears = get_transition_years(project_name)
+    transitionYears = get_transition_years(info)
 
     bands = range(0, len(transitionYears))
 
@@ -91,12 +89,12 @@ def interface():
     if colecao == "integracao":
         dir_src = info["folders"]["integracao"]
         dir_dst = info["folders"]["integracao_vrt"]
-        vrt_integration(dir_src, dir_dst)
+        vrt_integration(dir_src, dir_dst, info)
 
     if colecao == 'transicao':
         dir_src = info["folders"]["transicao"]
         dir_dst = info["folders"]["transicao_vrt"]
-        vrt_transition(dir_src, dir_dst, project_name)
+        vrt_transition(dir_src, dir_dst, info)
 
     if colecao == 'rgb':
         vrt_rgb(dir_src, dir_dst)
